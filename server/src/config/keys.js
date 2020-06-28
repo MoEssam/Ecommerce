f881e81e57b6d80d197cfd240a576f16d54f6bb3
Mohamed Essam <mohamedessam.moe@gmail.com>
2020-06-29 01:06:35 +0200

    Update keys.js

--- server/src/config/keys.js
+++ server/src/config/keys.js
@@ -1,9 +1,6 @@
 module.exports = {
-    googleclientID: "448479618084-fmktku8on23m9deu4suj2hhasn26r081.apps.googleusercontent.com",
-    googleclientSecret: "wN_n_iLD7Z4Qp9ivsIAsRBwV",
-    FACEBOOK_APP_ID: "687534178769299",
-    FACEBOOK_APP_SECRET: "7926b750c154ae90e202c751e0e32242",
-    callbackURL: 'http://localhost:5000/auth/google/callback',
-    callbackURL: 'http://localhost:5000/auth/facebook/callback',
-
+    googleclientID: "YOUR GOOGLE CLIENT ID",
+    googleclientSecret: "YOUR GOOGLE CLIENT SECRET",
+    FACEBOOK_APP_ID: "YOUR FACEBOOK APP ID",
+    FACEBOOK_APP_SECRET: "YOUR FACEBOOK APP SECRET",
 }
